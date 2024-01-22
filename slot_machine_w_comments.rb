3eee0aaf1588b68cd03a0b36203bd0020ce0dfc3
Emma A. A. Rünzel <emma@ruenzel.de>
2024-01-22 23:36:25 +0100

    Add TODOs to SlotMachine f/ Practice

---
+++ slot_machine_w_comments.rb
@@ -0,0 +1,11 @@
+# TODO: Create a constructor to initialize the SlotMachine class with an array of reels
+# TODO: Provide a default empty array for reels
+# TODO: Implement a method to play the slot machine
+# TODO: Generate random symbols for the reels and return the result
+# TODO: Implement a method to calculate the score based on the generated reels
+# TODO: Check for three identical symbols or two identical symbols with a joker (🃏)
+# TODO: and calculate the score accordingly.
+# TODO: Sorting the array of 3 symbols to make sure index 1 is one of the pair
+# TODO: Implement a private helper method to check for three identical symbols
+# TODO: Implement a private helper method to check for two identical symbols and a joker
+# TODO: Implement a private method to calculate the base score for a given symbol
