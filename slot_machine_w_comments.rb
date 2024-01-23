c72c74f0dad1563cd29e2f9f4a11f8c0c96c307c
Emma A. A. Rünzel <emma@ruenzel.de>
2024-01-23 16:17:38 +0100

    Add More Simple Code to Solve Challenge

--- slot_machine_w_comments.rb
+++ slot_machine_w_comments.rb
@@ -9,3 +9,41 @@
 # TODO: Implement a private helper method to check for three identical symbols
 # TODO: Implement a private helper method to check for two identical symbols and a joker
 # TODO: Implement a private method to calculate the base score for a given symbol
+
+class SlotMachine
+  SYMBOLS = [ "cherry",  "seven", "bell", "star", "joker"]
+  #               0         1       2       3       4
+  def initialize(reels = [])
+    @reels = reels
+  end
+
+  def play
+    @reels = []
+    3.times do
+      @reels << SYMBOLS.sample
+    end
+    return @reels
+  end
+
+  def score
+    if @reels.uniq.size == 1 # Three idendical
+      return base_score(@reels.first) * 10
+    elsif @reels.uniq.size == 2 && @reels.include?('🃏')
+      # calculate based on the scoring of column 2
+      return base_score(@reels.sort[1]) * 5
+    else
+      return 0
+    end
+  end
+
+  # calculate based on the scoring on column 1
+  # 3 Jokers => 50
+  # 3 Stars => 40
+  # 3 Bells => 30
+  # 3 Sevens => 20
+  # 3 Cherry => 10
+  def base_score(symbol)
+    SYMBOLS.index(symbol) + 1
+  end
+
+end
